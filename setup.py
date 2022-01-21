b4c76bdca43e50d2c06a1ebb60daed3534608b25
Chris La <woong@amazon.com>
2022-01-20 16:10:17 -0800

    Update for pypi release

--- setup.py
+++ setup.py
@@ -16,28 +16,58 @@
 """ This ensures modules and global scripts declared therein get installed
 ## See http://ros.org/doc/api/catkin/html/user_guide/setup_dot_py.html
 """
+import os
 from setuptools import find_packages
 from distutils.core import setup
 
 
-package_name = 'deepracer_env_config'
+def read(fname):
+    """
+    Args:
+        fname:
+    """
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+def read_version():
+    return read("VERSION").strip()
+
+
+package_name = "deepracer-env-config"
+
+# Declare minimal set for installation
+required_packages = [
+    "setuptools",
+    "pytest-flake8==1.0.7",
+    "pytest-pep257==0.0.5",
+    "pytest-timeout==1.4.2",
+]
 
 setup_args = {
     "name": package_name,
-    "version": '0.0.0',
-    "packages": find_packages(where='.', exclude='test'),
-    "package_dir": {'': '.'},
-    "install_requires": [
-        'setuptools',
-        'pytest-flake8==1.0.7',
-        'pytest-pep257==0.0.5',
-        'pytest-timeout==1.4.2'
-    ]
+    "version": read_version(),
+    "packages": find_packages(where=".", exclude="test"),
+    "package_dir": {"": "."},
+    "description": "Open source library to change DeepRacer environment configuration dynamically.",
+    "long_description": read("README.md"),
+    "author": "Amazon Web Services",
+    "url": "https://github.com/aws-deepracer/deepracer-env-config/",
+    "license": "Apache License 2.0",
+    "keywords": "ML RL Amazon AWS AI DeepRacer",
+    "classifiers": [
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+    ],
+    "install_requires": required_packages,
 }
 
 try:
     from catkin_pkg.python_setup import generate_distutils_setup
-
+    setup_args["name"] = "deepracer_env_config"
     PACKAGE_IMPORT = generate_distutils_setup(**setup_args)
 
     setup(**PACKAGE_IMPORT)
